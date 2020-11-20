011692339261ec5cf31a1eb2a84b7bb74549f988
minie12 <ejunglim12@gmail.com>
2020-11-20 13:44:43 +0900

    Up Down example

--- iiwa_examples/src/updown.cpp
+++ iiwa_examples/src/updown.cpp
@@ -7,6 +7,7 @@
 
 geometry_msgs::PoseStamped current_pose;
 
+// get current pose of robot in gazebo (not used for real robot)
 void chatterCallback(const geometry_msgs::PoseStamped::ConstPtr& msg){
   current_pose = *msg;
 }
@@ -17,37 +18,54 @@ int main (int argc, char **argv) {
   ros::init(argc, argv, "CommandRobot");
   ros::NodeHandle nh("~");
 
-  //iiwa_ros::state::CartesianPose iiwa_pose_state;
   iiwa_ros::command::CartesianPose iiwa_pose_command;
-  //iiwa_pose_state.init("iiwa");
   iiwa_pose_command.init("iiwa");
 
+  bool sim;
+  nh.param("sim", sim, true);
+
+  iiwa_ros::state::CartesianPose iiwa_pose_state;
   iiwa_msgs::CartesianPose command_cartesian_position;
+  ros::Subscriber sub;
+
+  if(sim == false){ // if real robot is operated
+    iiwa_pose_state.init("iiwa");
+    std::cout << "initialized" << std::endl;
+  }
+  else {
+    sub = nh.subscribe("/iiwa/state/CartesianPose", 5, chatterCallback);
+    std::cout << "subscribed" << std::endl;
+  }
 
   // ROS spinner.
   ros::AsyncSpinner spinner(1);
   spinner.start();
 
-  ros::Subscriber sub = nh.subscribe("/iiwa/state/CartesianPose", 1000, chatterCallback);
-
   // Dynamic parameter to choose the rate at wich this node should run
   double ros_rate;
   nh.param("ros_rate", ros_rate, 0.1); // 0.1 Hz = 10 seconds
   ros::Rate* loop_rate_ = new ros::Rate(ros_rate);
-  int direction = 0.2;
+  int direction = 1;
+
 
   while(ros::ok()){
-    ros::Duration(2.0).sleep();
-    //command_cartesian_position = iiwa_pose_state.getPose();
+    loop_rate_->sleep();
 
-    std::cout << "POSE: \n" << current_pose.pose.position << std::endl;
-    current_pose.pose.position.z -= direction * 0.10;
-    iiwa_pose_command.setPose(command_cartesian_position.poseStamped);
+    if(sim == false){ // if real robot is operated
+      command_cartesian_position = iiwa_pose_state.getPose();
+      command_cartesian_position.poseStamped.pose.position.z -= direction * 0.10;
+      iiwa_pose_command.setPose(command_cartesian_position.poseStamped);
 
-    std::cout << "-------------------" << std::endl;
+      std::cout << "COMMAND PUBLISHED: false" << std::endl;
+    }
+    else{
+      current_pose.pose.position.z -= direction * 0.10;
+      iiwa_pose_command.setPose(current_pose);
+
+      std::cout << "COMMAND PUBLISHED: true" << std::endl;
+    }
 
     direction *= -1;
-	loop_rate_->sleep();
   }
 
   /*
